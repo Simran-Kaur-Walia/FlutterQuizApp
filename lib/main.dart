9718ce76f0084d610ee201bfd603f3692faca685
software-101 <simran0502kaur@gmail.com>
2021-07-19 09:15:03 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite programming language?',
+      'answers': [
+        {'text': 'C++', 'score': 3},
+        {'text': 'DART', 'score': 11},
+        {'text': 'Python', 'score': 5},
+        {'text': 'JavaScript', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite scientist?',
+      'answers': [
+        {'text': 'Nikola Tesla', 'score': 1},
+        {'text': 'Albert Einstein', 'score': 1},
+        {'text': 'Leonardo da Vinci', 'score': 1},
+        {'text': 'Isaac Newton', 'score': 1},
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        //backgroundColor: Color(0xff191919),
+        appBar: AppBar(
+          title: Text('Flutter Quiz App'),
+          backgroundColor: Color(0xff8B008B),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
